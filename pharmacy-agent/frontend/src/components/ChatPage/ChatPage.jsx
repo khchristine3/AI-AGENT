c08178a88380c6fbd26f507c82ca4f196684fa3e
khchristine3 <khchristine3@gmail.com>
2025-12-20 18:31:02 +0200

    created ChatWindow component also, message buuble component and tested with dummy msgs for now

--- pharmacy-agent/frontend/src/components/ChatPage/ChatPage.jsx
+++ pharmacy-agent/frontend/src/components/ChatPage/ChatPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Title from "./Title/Title.jsx";
+import ChatWindow from "./ChatWindow/ChatWindow.jsx";
 import './ChatPage.css';
 
 function ChatPage() {
   return (
     <div className="ChatPage">
       <Title text="🏥 Pharmacy Assistant" />
+      <ChatWindow />
+
     </div>
   );
 }
