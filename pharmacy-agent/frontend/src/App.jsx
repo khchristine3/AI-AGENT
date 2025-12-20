d359dd0d9121a4c212dae4cc335a20283c47b375
khchristine3 <khchristine3@gmail.com>
2025-12-20 14:16:06 +0200

    Refactor: Moved title from App to reusable Title component in ChatPage

--- pharmacy-agent/frontend/src/App.jsx
+++ pharmacy-agent/frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import './App.css'
-
+import ChatPage from "./components/ChatPage/ChatPage.jsx";
+import './App.css';
 
 function App() {
-
-
   return (
-    <div className="AppTitle">
-      <h1>🏥 Pharmacy Assistant</h1>
+    <div className="App">
+      <ChatPage />
     </div>
   );
 }
